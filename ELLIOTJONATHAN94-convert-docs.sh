7afed35454ba7e9749f7888a6bc9af59ac15f477
elliotj <elliotj@live.unc.edu>
2017-07-17 12:56:28 -0400

    Text Conversion through Bash scripting

--- ELLIOTJONATHAN94-convert-docs.sh
+++ ELLIOTJONATHAN94-convert-docs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Declare input argument as a variable
-INPUTFILE=OriginalDoucment1.md 
+INPUTFILE=OriginalDocument1.md 
 # Convert markdown to HTML
-pandoc -o OriginalDoucment1.html OriginalDoucment1.md
+pandoc -o OriginalDocument1.html OriginalDocument1.md
 # Convert markdown to DOCX
-pandoc -o OriginalDoucment1.docx OriginalDoucment1.md  
+pandoc -o OriginalDocument1.docx OriginalDocument1.md  
 # Convert markdown to ODT
-pandoc -o OriginalDoucment1.odt OriginalDoucment1.md
+pandoc -o OriginalDocument1.odt OriginalDocument1.md
 # Convert markdown to PDF
-pandoc -o OriginalDoucment1.pdf OriginalDoucment1.md
+pandoc -o OriginalDocument1.pdf OriginalDocument1.md
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted OriginialDocument1 to HTML, DOCX, ODT, PDF"
